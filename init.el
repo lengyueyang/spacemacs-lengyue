e0e3524a4b43b406ad54ed39996a164efd1f6475
lengyue <maoxiaoweihl@gmail.com>
2016-11-05 22:57:45 +0800

    Add basic init.el configuration

--- init.el
+++ init.el
@@ -24,29 +24,41 @@ values."
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
      (auto-completion :variables
-                      auto-completion-return-key-behavior 'complete
-                      auto-completion-tab-key-behavior 'cycle
-                      auto-completion-complete-with-key-sequence nil
-                      auto-completion-complete-with-key-sequence-delay 0
-                      auto-completion-private-snippets-directory nil
+                      ;;                 auto-completion-return-key-behavior 'complete
+                      ;;                 auto-completion-tab-key-behavior 'cycle
+                      ;;                 auto-completion-complete-with-key-sequence nil
+                      ;;                 auto-completion-complete-with-key-sequence-delay 0
+                      ;;                 auto-completion-private-snippets-directory nil
                       auto-completion-enable-snippets-in-popup t
-                      auto-completion-enable-help-tooltip t
-                      auto-completion-enable-sort-by-usage t)
+                      ;;                 auto-completion-enable-help-tooltip t
+                      auto-completion-enable-sort-by-usage t
+                      :disabled-for org markdown)
      better-defaults
-     chinese
+     (chinese :packages youdao-dictionary
+              :variables chinese-enable-youdao-dict t)
      emacs-lisp
-     git
+     (git :variables
+          git-magit-status-fullscreen t
+          magit-push-always-verify nil
+          magit-save-repository-buffers 'dontask
+          magit-revert-buffers 'silent
+          magit-refs-show-commit-count 'all
+          magit-revision-show-gravatars nil)
      github
      javascript
+     (typescript :variables
+                 typescript-fmt-on-save nil
+                 typescript-fmt-tool 'typescript-formatter)
      bibtex
      ivy
      yaml
      (latex :variables latex-enable-auto-fill t)
-     c-c++
+     (c-c++ :variables
+            c-c++-default-mode-for-headers 'c++-mode)
      (ess :variables ess-enable-smart-equals t)
      racket
      (python :variables
-             python-test-runner 'node
+             python-test-runner '(nose pytest)
              python-enable-yapf-format-on-save t)
      ipython-notebook
      markdown
@@ -57,27 +69,46 @@ values."
                       syntax-checking-enable-by-default t
                       syntax-checking-enable-tooltips t)
      (spell-checking :variables
-                     ;;"spell-checking-enable-auto-dictionary t"
                      spell-checking-enable-by-default t)
      version-control
      (colors :variables
              colors-enable-nyan-cat-progress-bar t)
-     lengyueyang
+
      pandoc
-     emacs-lisp
      semantic
      graphviz
-     ;;zilongshanren
+     (ranger :variables ranger-show-preview t)
+     pdf-tools
+     prodigy
+     search-engine
+     (vinegar :variables vinegar-reuse-dired-buffer t)
+     (spacemacs-layouts :variables layouts-enable-autosave nil
+                        layouts-autosave-delay 300)
+     (ibuffer :variables ibuffer-group-buffers-by 'projects)
+     restclient
+     (gtags :disabled-for clojure emacs-lisp javascript latex python shell-scripts ess)
+     docker
+     deft
+     shaders
+     react
+     (ruby :variables ruby-version-manager 'chruby)
+     ruby-on-rails
+     lua
+     html
+     (clojure :variables clojure-enable-fancify-symbols t)
      (elfeed :variables
              ;;elfeed-enable-web-interface t
              rmh-elfeed-org-files (list "~/Emacs-lengyue/Wiki-lengyue/Elfeed.org"))
+     lengyueyang
+
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages then consider to create a layer, you can also put the
    ;; configuration in `dotspacemacs/config'.
    dotspacemacs-additional-packages '(nodejs-repl
-                                      youdao-dictionary)
+                                      youdao-dictionary
+                                      chinese-yasdcv)
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
@@ -267,7 +298,7 @@ values."
   (setq org-global-properties (quote (("Effort_ALL" . "0:15 0:30 0:45 1:00 2:00 3:00 4:00 5:00 6:00 0:00")
                                       ("STYLE_ALL" . "habit"))))
 
-
+  (require 'chinese-yasdcv)
 
 
   (setq org-confirm-babel-evaluate nil)
@@ -594,29 +625,8 @@ layers configuration. You are free to put any user code."
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-engine (quote xetex))
- '(custom-safe-themes
-   (quote
-    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
- '(magit-log-section-arguments (quote ("--decorate" "-n256")))
- '(org-agenda-files
-   (quote
-    ("/media/My Data/DCH/CPG/肺癌临床路径/肺癌临床路径.org" "/home/lengyue/Emacs-lengyue/GTD-lengyue/GTD-lengyue.org")))
- '(org-pomodoro-length 1)
- '(org-pomodoro-short-break-length 1)
- '(package-selected-packages
-   (quote
-    (yaml-mode auto-complete youdao-dictionary names chinese-word-at-point smooth-scrolling py-yapf page-break-lines org-repo-todo leuven-theme helm-themes helm-swoop helm-pydoc helm-projectile helm-mode-manager helm-gitignore helm-flyspell helm-flx helm-descbinds helm-company helm-c-yasnippet helm-ag fcitx buffer-move bracketed-paste ace-jump-helm-line swiper auctex anzu undo-tree flyspell-correct dash ht ivy-purpose window-purpose imenu-list hide-comnt anaconda-mode yasnippet magit-popup git-commit async f s with-editor skewer-mode fringe-helper git-gutter+ git-gutter julia-mode noflet powerline elfeed websocket chinese-pyim-basedict biblio-core counsel ess smartparens flycheck projectile magit js2-mode ivy graphviz-dot-mode xterm-color ws-butler window-numbering which-key volatile-highlights use-package toc-org spaceline shell-pop restart-emacs rainbow-delimiters racket-mode pyvenv py-isort pip-requirements persp-mode paradox pandoc-mode ox-pandoc orgit org-ref org-pomodoro alert org-plus-contrib open-junk-file neotree multi-term move-text markdown-toc markdown-mode magit-gitflow macrostep js2-refactor info+ indent-guide ido-vertical-mode hl-todo highlight-numbers help-fns+ helm-make helm-bibtex helm helm-core google-translate github-clone gitconfig-mode git-timemachine git-messenger git-link expand-region exec-path-from-shell evil-unimpaired evil-surround evil-search-highlight-persist evil-nerd-commenter evil-mc evil-matchit evil-magit evil-iedit-state iedit evil-exchange evil-escape eshell-prompt-extras elfeed-web elfeed-org org yapfify wgrep web-beautify vi-tilde-fringe uuidgen uimage stickyfunc-enhance srefactor spinner spacemacs-theme smex smeargle rainbow-mode rainbow-identifiers quelpa pytest pyenv-mode pcre2el parsebib parent-mode pangu-spacing org-projectile org-present org-download org-caldav org-bullets nodejs-repl mwim multiple-cursors mmm-mode magit-gh-pulls lorem-ipsum log4e livid-mode live-py-mode linum-relative link-hint key-chord json-mode js-doc ivy-hydra hy-mode hungry-delete htmlize hl-sexp highlight-parentheses highlight-indentation golden-ratio gnuplot gntp gitignore-mode github-search github-browse-file gitattributes-mode git-gutter-fringe git-gutter-fringe+ gist gh-md flyspell-correct-ivy flycheck-pos-tip flx-ido find-by-pinyin-dired fill-column-indicator fancy-battery faceup eyebrowse evil-visualstar evil-visual-mark-mode evil-tutor evil-numbers evil-lisp-state evil-indent-plus evil-ediff evil-args evil-anzu eval-sexp-fu ess-smart-equals ess-R-object-popup ess-R-data-view eshell-z esh-help elisp-slime-nav elfeed-goodies ein dumb-jump disaster diminish diff-hl define-word cython-mode counsel-projectile company-tern company-statistics company-quickhelp company-c-headers company-auctex company-anaconda column-enforce-mode color-identifiers-mode coffee-mode cmake-mode clean-aindent-mode clang-format chinese-pyim bind-key biblio auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile auctex-latexmk aggressive-indent adaptive-wrap ace-window ace-pinyin ace-link ac-ispell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
- '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
+
+(setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
+(load custom-file 'no-error 'no-message)
+
 

0bdeef1b534a8a9e8c9edbbb9a9704c7378b0206
lengyue <maoxiaoweihl@gmail.com>
2017-08-08 22:56:10 +0800

    Change large file size to 50M

--- lengyueyang.el
+++ lengyueyang.el
@@ -272,7 +272,7 @@
 
 (setq large-file-warning-threshold 300000000)
 (defun spacemacs/check-large-file ()
-  (when (> (buffer-size) 500000)
+  (when (> (buffer-size) 50000000)
     (progn (fundamental-mode)
            (hl-line-mode -1)))
   (if (and (executable-find "wc")

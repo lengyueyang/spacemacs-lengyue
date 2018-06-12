aa0aa3372d3b02bd146e443bb460ef9c3b280ba2
lengyue <maoxiaoweihl@gmail.com>
2018-06-12 12:51:14 +0800

    Add python-mode snippets

--- lengyueyang.el
+++ lengyueyang.el
@@ -871,9 +871,6 @@ add an entry to the end of it."
 
 (add-to-load-path "~/.spacemacs.d/package/ox-ipynb")
 (require 'ox-ipynb)
-(setq
- x-select-enable-clipboard nil
- x-select-enable-primary t)
 
 ;; (add-to-load-path "~/.spacemacs.d/package/org-edit-latex")
 (require 'org-edit-latex)
